79fb5c38339b20e42c02468ce6369a01f8b338f1
Ruan Ventura <iamruanmv@gmail.com>
2026-06-23 00:28:22 -0400

    Del ValuesController Add NewsController Add BaseEntity Add News

---
+++ RestFull/API/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
+    {
+        private readonly ILogger<NewsController> _logger;
+
+        public NewsController(ILogger<NewsController> logger)
+        { 
+            _logger = logger;
+        }
+    }
+}
