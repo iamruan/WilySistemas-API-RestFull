74c97ed537cc5b2fae06335a05b09ada69ddb657
Ruan Ventura <iamruanmv@gmail.com>
2026-06-30 01:11:10 -0400

    Add Services

---
+++ RestFull/API/Services/NewsService.cs
@@ -0,0 +1,18 @@
+﻿using API.Entities;
+using API.Infra;
+using AutoMapper;
+
+namespace API.Services
+{
+    public class NewsService
+    {
+        private readonly IMapper _mapper;
+        private readonly IMongoRepository<News> _news;
+
+        public NewsService(IMongoRepository<News> news, IMapper mapper)
+        {
+            _mapper = mapper;
+            _news = news;
+        }
+    }
+}
--- RestFull/API/Startup.cs
+++ RestFull/API/Startup.cs
@@ -2,20 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Infra;
 using API.Mappers;
+using API.Services;
+using AutoMapper.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace API
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -23,6 +31,16 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Rest WilySistemas", Version = "v1" });
             });
+
+            #region [Database]
+            services.Configure<DatabaseSettings>(Configuration.GetSection(nameof(DatabaseSettings)));
+            services.AddSingleton<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
+            #endregion
+
+            #region [DI]
+            services.AddSingleton(typeof(IMongoRepository<>), typeof(MongoRepository<>));
+            services.AddSingleton<NewsService>();
+            #endregion
 
             #region [AutoMapper]
             services.AddAutoMapper(typeof(EntityToViewModelMapping), typeof(ViewModelToEntityMapping));
@@ -41,6 +59,7 @@ namespace API
             }
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
